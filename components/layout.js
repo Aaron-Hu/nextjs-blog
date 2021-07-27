ca6bd4b7b99fe48e7c10a14080779ee9d4652aa8
huxuerui <bjhxue@cn.ibm.com>
2021-07-27 11:11:50 +0800

    update page title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Your Name'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Nextjs-blog'
 
 
 export default function Layout({ children, backPath }) {
